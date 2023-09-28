e93cb33c36373280e574dc588914844cc47f6b10
kaami419 <tareen544@gmail.com>
2023-09-28 12:38:20 +0500

    removed csv from role seeder

--- controllers/Seeder/roleSeeder.js
+++ controllers/Seeder/roleSeeder.js
@@ -1,21 +1,68 @@
+// const rolemodel = require("../../models/role");
+// const csvtojson = require("csvtojson");
+
+// exports.roleSeeder = async function (req, res) {
+//     try {
+//         const roleData = await rolemodel.find({});
+//       //  console.log(roleData);
+
+//         const count = await rolemodel.count();
+//      //   console.log(count);
+        
+//         if (roleData == null) {
+//             return res.status(200).json({
+//                 message: "role list Data",
+//                 roleData,
+//             });
+//         }
+//         else if (roleData == "") {
+//             const seedData = [ 
+//                 {
+//                     role: 'admin'
+//                 },
+//                 {
+//                     role: 'seller'
+//                 },
+//                 {
+//                     role: 'buyer'
+//                 },
+//               ];
+//             csvtojson()
+//                 .fromFile("public/role.csv")
+//                 .then(async (csvData) => {
+//                     // console.log(csvData);
+//                     await rolemodel
+//                         .insertMany(seedData)
+//                         .then((res1) => {
+//                             // console.log(csvData);
+//                             return res.status(200).json({
+//                                 message: "role added successfuly!",
+//                                 // countryCsvData,
+//                             });
+//                         });
+//                 });
+//         }else{
+//             return res.status(200).json({
+//                 message: "role already exist",
+//                 roleData,
+//             });
+//         }
+//     } catch (error) {
+//         console.log("---------", error);
+//         return res.status(500).json({
+//             error:
+//                 "Something went wrong which is why category collection is not working",
+//         });
+//     }
+// };
+
 const rolemodel = require("../../models/role");
-const csvtojson = require("csvtojson");
 
 exports.roleSeeder = async function (req, res) {
     try {
         const roleData = await rolemodel.find({});
-      //  console.log(roleData);
 
-        const count = await rolemodel.count();
-     //   console.log(count);
-        
-        if (roleData == null) {
-            return res.status(200).json({
-                message: "role list Data",
-                roleData,
-            });
-        }
-        else if (roleData == "") {
+        if (roleData == null || roleData.length === 0) {
             const seedData = [ 
                 {
                     role: 'admin'
@@ -26,32 +73,23 @@ exports.roleSeeder = async function (req, res) {
                 {
                     role: 'buyer'
                 },
-              ];
-            csvtojson()
-                .fromFile("public/role.csv")
-                .then(async (csvData) => {
-                    // console.log(csvData);
-                    await rolemodel
-                        .insertMany(seedData)
-                        .then((res1) => {
-                            // console.log(csvData);
-                            return res.status(200).json({
-                                message: "role added successfuly!",
-                                // countryCsvData,
-                            });
-                        });
-                });
-        }else{
+            ];
+
+            await rolemodel.insertMany(seedData);
+
             return res.status(200).json({
-                message: "role already exist",
+                message: "Roles added successfully!",
+            });
+        } else {
+            return res.status(200).json({
+                message: "Roles already exist",
                 roleData,
             });
         }
     } catch (error) {
         console.log("---------", error);
         return res.status(500).json({
-            error:
-                "Something went wrong which is why category collection is not working",
+            error: "Something went wrong, and the role collection is not working",
         });
     }
 };
