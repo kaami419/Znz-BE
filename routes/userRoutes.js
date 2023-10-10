c3fd1180b8bc98dcd6a8e042f792fb20c7b8efeb
kaami419 <tareen544@gmail.com>
2023-10-10 11:25:13 +0500

    fixed forget password

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,26 +1,34 @@
-const express=require("express");
-const userRoutes=require("../controllers/Users/userController");
-const mailConfirmation=require("../controllers/Emails/email");
-const protect=require("../middleware/authMiddleware");
+const express = require("express");
+const userRoutes = require("../controllers/Users/userController");
+const mailConfirmation = require("../controllers/Emails/email");
+const protect = require("../middleware/authMiddleware");
 const router = express.Router();
 
 /// User///
-router.post("/uploadImage",userRoutes.uploadImage);
-router.post("/UserRegister",userRoutes.userRegister);
-router.get("/getUser/:id",protect.verifyToken,userRoutes.getUser);
-router.get("/getAllUser",userRoutes.getAllUser);
-router.delete("/deleteUser/:id",protect.verifyToken,userRoutes.deleteUser);
-router.put("/UserUpdate/:id",protect.verifyToken,userRoutes.userUpdate);
-router.get("/getTopSeller",userRoutes.getTopSeller);
-router.get("/userSearch",userRoutes.userSearch);
+router.post("/uploadImage", userRoutes.uploadImage);
+router.post("/UserRegister", userRoutes.userRegister);
+router.get("/getUser/:id", protect.verifyToken, userRoutes.getUser);
+router.get("/getAllUser", userRoutes.getAllUser);
+router.delete("/deleteUser/:id", protect.verifyToken, userRoutes.deleteUser);
+router.put("/UserUpdate/:id", protect.verifyToken, userRoutes.userUpdate);
+router.get("/getTopSeller", userRoutes.getTopSeller);
+router.get("/userSearch", userRoutes.userSearch);
 //Auth//
-router.post("/UserLogin",userRoutes.userLogin);
+router.post("/UserLogin", userRoutes.userLogin);
 
 // mail
 
-router.get("/forUserEmail",mailConfirmation.forUserEmail);
-router.get("/userEmailVerification/:id",mailConfirmation.userEmailVerification);
-router.get("/forUserConfirmationEmail/:userEmail",mailConfirmation.forUserConfirmationEmail);
-router.post("/EmailForForgetPassword",mailConfirmation.EmailForForgetPassword);
-router.post("/forgetPassword/:id",mailConfirmation.forgetPassword);
+router.get("/forUserEmail", mailConfirmation.forUserEmail);
+router.get(
+  "/userEmailVerification/:id",
+  mailConfirmation.userEmailVerification
+);
+router.get(
+  "/forUserConfirmationEmail/:userEmail",
+  mailConfirmation.forUserConfirmationEmail
+);
+router.post("/EmailForForgetPassword", mailConfirmation.EmailForForgetPassword);
+router.post("/forgetPassword/:id", mailConfirmation.forgetPassword);
+router.post("/forgetPassword", mailConfirmation.forgetPassword);
+
 module.exports = router;
